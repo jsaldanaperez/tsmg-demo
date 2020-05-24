d283f63a52a3ebe6104b860a2c653882386e149c
jperez <j.saldanaperez@gmail.com>
2020-05-24 11:10:01 +0200

    updated packages and changed demo to ts definitions

--- WebApi/WebApi/TypeScriptGeneration.cs
+++ WebApi/WebApi/TypeScriptGeneration.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TypeScriptModelsGenerator;
+using TypeScriptModelsGenerator.Definitions;
 using TypeScriptModelsGenerator.HttpServices;
 using TypeScriptModelsGenerator.Options;
 
@@ -12,35 +13,32 @@ namespace WebApi
     {
         private static DirectoryInfo RootDirectory => new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent
             .Parent.Parent.Parent.Parent;
-        
+
         public static void Execute()
         {
-            var path = RootDirectory;
             var controllers = typeof(TypeScriptGeneration).Assembly.ExportedTypes
                 .Where(x => x.BaseType == typeof(ControllerBase)).ToList();
-
             var destination = Path.Combine(RootDirectory.FullName, "Website", "src", "app");
-            
+
+            var typeScriptDefinition = TypeScriptDefinitionFactory.Create()
+                .Folder("services", servicesFolder => servicesFolder.HttpServices(controllers, serviceBuilder =>
+                    serviceBuilder
+                        .WithContent(content =>
+                            content
+                                .AddImport("import { HttpClient } from '@angular/common/http';")
+                                .AddImport("import { Injectable } from '@angular/core';")
+                                .AddImport("import { Observable } from 'rxjs';")
+                                .AddDecorator("@Injectable({ providedIn: 'root' })")
+                        )));
+
             TypeScriptModelsGeneration
-                .Setup(controllers, destination, (options) =>
+                .Setup(typeScriptDefinition, destination, (options) =>
                 {
                     options.GenerationMode = GenerationMode.Crawl;
                     options.AddNamespaceReplaceRule(nameof(WebApi), "models");
-                    options.AddHttpServices(template =>
-                    {
-                        template.Imports = x =>
-                            new[]
-                            {
-                                "import { HttpClient } from '@angular/common/http';",
-                                "import { Injectable } from '@angular/core';",
-                                "import { Observable } from 'rxjs';"
-                            };
-                        template.Decorators = x => new[] {"@Injectable({ providedIn: 'root' })"};
-                    });
                     options.CleanDestinationDirectories.AddRange(new []{"models", "services"});
                 })
                 .Execute();
-                
         }
     }
 }
